f40135c71754bc6e296643b51482cd27419c580c
jaina-vaish <jainavaishnavi44@gmail.com>
2026-08-02 19:45:30 +0530

    Time: 4 ms (10.03%) | Memory: 43.7 MB (6.47%) - LeetSync

---
+++ 822-unique-morse-code-words/unique-morse-code-words.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        HashMap<Character, String> mp = new HashMap<>();
+        mp.put('a', ".-");
+mp.put('b', "-...");
+mp.put('c', "-.-.");
+mp.put('d', "-..");
+mp.put('e', ".");
+mp.put('f', "..-.");
+mp.put('g', "--.");
+mp.put('h', "....");
+mp.put('i', "..");
+mp.put('j', ".---");
+mp.put('k', "-.-");
+mp.put('l', ".-..");
+mp.put('m', "--");
+mp.put('n', "-.");
+mp.put('o', "---");
+mp.put('p', ".--.");
+mp.put('q', "--.-");
+mp.put('r', ".-.");
+mp.put('s', "...");
+mp.put('t', "-");
+mp.put('u', "..-");
+mp.put('v', "...-");
+mp.put('w', ".--");
+mp.put('x', "-..-");
+mp.put('y', "-.--");
+mp.put('z', "--..");
+
+
+    for(int i=0;i<words.length;i++)
+    {
+        String str = words[i];
+        String res="";
+        for(int j=0;j<str.length();j++)
+        {
+            char ch = str.charAt(j);
+            res+=mp.get(ch);
+        }
+        words[i] = res;
+    }
+    HashSet<String> set = new HashSet<>();
+    for(int i=0;i<words.length;i++)
+    {
+       set.add(words[i]);
+    }
+    return set.size();
+    }
+}
